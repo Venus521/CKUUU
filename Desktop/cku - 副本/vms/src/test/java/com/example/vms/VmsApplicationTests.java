1f79608c59f2efac6d6cc42179e72bb93d2dd1a3
Venus <1045466986@qq.com>
2025-02-17 22:12:54 +0800

    Initial commit: Upload files from local folder

---
+++ Desktop/cku - 副本/vms/src/test/java/com/example/vms/VmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vms;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+class VmsApplicationTests {
+@SpringBootTest
+}
+	void contextLoads() {
+	@Test
+	}
+
+
+
+
